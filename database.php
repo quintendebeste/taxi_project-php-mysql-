e01a6d0b01ba4bba644988584d682b913b96542e
lars <larsdeboer22@gmail.com>
2023-01-11 13:09:06 +0100

    database naam aangepast

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 $host  = "localhost";
 $dbuser = "root";
 $dbpass = "";
-$dbname = "k";
+$dbname = "taxibedrijf";
 
 // Maak een  database connectie
 $conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
