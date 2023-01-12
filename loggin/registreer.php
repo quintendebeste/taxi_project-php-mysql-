b805826651f30e49c8f5ff3d993b83036c637e69
Kay Stolk <186763@novacollege.nl>
2023-01-12 11:19:40 +0100

    css

--- loggin/registreer.php
+++ loggin/registreer.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="../style.css">
     <title>Document</title>
 </head>
 <body>
@@ -18,7 +18,7 @@
         <input type="password" name="password" placeholder="Wachtwoord" required>
         <input type="submit" name="submit" value="Registreren">
     </form>
-    <form id="login-form" action="index.php" method="post">
+    <form id="login-form" action="../index.php" method="post">
         <input type="submit" value="Login">
     </form>
 </body>
