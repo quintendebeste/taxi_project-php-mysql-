5512a69200fd6270ed9fa60423a798a43536d446
Quinten Vedder <quintenvedder@gmail.com>
2023-01-13 09:57:47 +0100

    sessions bug fix

--- functions.php
+++ functions.php
@@ -1,27 +1,6 @@
 <?php
-
-if($_GET["function"] == "ritaanvragen"){
-    function ritaanvragen(){
-        require 'database.php';
-        var_dump($_POST);die;
-
-    $number_of_passengers = $_POST["number_of_passengers"];
-    $pickup_datetime = $_POST["pickup_datetime"];
-    $pickup_adress = $_POST["pickup_adress"];
-    $pickup_city = $_POST["pickup_city"];
-    $destination_datetime = $_POST["destination_datetime"];
-    $destination_adress = $_POST["destination_adress"];
-    $destination_city = $_POST["destination_city"];
-
-    $sql = "INSERT INTO rides (number_of_passengers,pickup_datetime ,pickup_address ,pickup_city ,destination_datetime,destination_address,destination_city )
-            VALUES ('$number_of_passengers', '$pickup_datetime','$pickup_adress', '$pickup_city', '$destination_datetime' , '$destination_adress', '$destination_city')";
-
-    mysqli_query($mysqli, $sql);
-
-    header("location: klant/homepage.php");
-
-    mysqli_close($mysqli);
-    }
+if(!empty($_GET) && $_GET["function"] == "ritaanvragen"){
+    ritaanvragen();
 }
 
 if(!empty($_GET)){
@@ -51,4 +30,31 @@ function deleteData($id, $table)
     mysqli_query($conn, $sql);
 
     header("location: overzicht-auto.php");
+}
+
+function ritaanvragen(){
+    require 'database.php';
+    var_dump($_POST);
+
+    $number_of_passengers = $_POST["num_passagiers"];
+    $pickup_datetime = $_POST["pickup_datetime"];
+    $pickup_adress = $_POST["pickup_address"];
+    $pickup_city = $_POST["pickup_city"];
+    $destination_adress = $_POST["destination_address"];
+    $destination_city = $_POST["destination_city"];
+    
+    $datetime = new DateTime($pickup_datetime);
+    $datetime->modify('+5 minutes');
+    $new_datetime = $datetime->format('Y-m-d H:i:s');
+    
+    $destination_datetime = $new_datetime;
+
+    $sql = "INSERT INTO rides (number_of_passengers,pickup_datetime ,pickup_address ,pickup_city ,destination_datetime,destination_address,destination_city )
+            VALUES ('$number_of_passengers', '$pickup_datetime','$pickup_adress', '$pickup_city', '$destination_datetime' , '$destination_adress', '$destination_city')";
+
+    mysqli_query($conn, $sql);
+
+    header("location: klant/homepage.php");
+
+    mysqli_close($mysqli);
 }
